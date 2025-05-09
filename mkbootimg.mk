4c45de00e93935184e390321b62c01c7dd0f8330
strobecat <203321455+strobecat@users.noreply.github.com>
2025-05-09 23:09:51 +0800

    try build normal image first

--- mkbootimg.mk
+++ mkbootimg.mk
@@ -19,26 +19,26 @@ define make_header
   perl -e 'print pack("a4 L a32 a472", "\x88\x16\x88\x58", $$ARGV[0], $$ARGV[1], "\xFF"x472)' $(1) $(2) > $(3)
 endef
 
-$(recovery_kernel).mtk.header: $(recovery_kernel)
+$(recovery_kernel)-mtk.header: $(recovery_kernel)
 	size=$$($(call get-file-size,$(recovery_kernel))); \
 		$(call make_header, $$((size)), "KERNEL", $@)
 
-$(recovery_kernel).mtk: $(recovery_kernel).mtk.header
+$(recovery_kernel)-mtk: $(recovery_kernel)-mtk.header
 	$(call pretty,"Adding MTK header to recovery kernel.")
-	$(hide) cat $(recovery_kernel).mtk.header $(recovery_kernel) > $(recovery_kernel).mtk
+	cat $(recovery_kernel)-mtk.header $(recovery_kernel) > $(recovery_kernel)-mtk
 
-$(recovery_ramdisk).mtk.header: $(recovery_ramdisk)
+$(recovery_ramdisk)-mtk.header: $(recovery_ramdisk)
 	size=$$($(call get-file-size,$(recovery_ramdisk))); \
 		$(call make_header, $$((size)), "ROOTFS", $@)
 
-$(recovery_ramdisk).mtk: $(recovery_ramdisk).mtk.header
+$(recovery_ramdisk)-mtk: $(recovery_ramdisk)-mtk.header
 	$(call pretty,"Adding MTK header to recovery ramdisk.")
-	$(hide) cat $(recovery_ramdisk).mtk.header $(recovery_ramdisk) > $(recovery_ramdisk).mtk
+	cat $(recovery_ramdisk)-mtk.header $(recovery_ramdisk) > $(recovery_ramdisk)-mtk
 
 INTERNAL_MTK_RECOVERYIMAGE_ARGS := \
 	$(addprefix --second ,$(INSTALLED_2NDBOOTLOADER_TARGET)) \
-	--kernel $(recovery_kernel).mtk \
-	--ramdisk $(recovery_ramdisk).mtk
+	--kernel $(recovery_kernel)-mtk \
+	--ramdisk $(recovery_ramdisk)-mtk
 
 ifdef BOARD_KERNEL_CMDLINE
   INTERNAL_MTK_RECOVERYIMAGE_ARGS += --cmdline "$(BOARD_KERNEL_CMDLINE)"
@@ -52,8 +52,8 @@ ifdef BOARD_KERNEL_PAGESIZE
 endif
 
 $(INSTALLED_RECOVERYIMAGE_TARGET): $(MKBOOTIMG) \
-		$(recovery_ramdisk).mtk \
-	  $(recovery_kernel).mtk	
+		$(recovery_ramdisk)-mtk \
+	  $(recovery_kernel)-mtk	
 	@echo -e ${PRT_IMG}"----- Making recovery image ------"${CL_RST}
 	$(hide) $(MKBOOTIMG) $(INTERNAL_MTK_RECOVERYIMAGE_ARGS) $(BOARD_MKBOOTIMG_ARGS) --output $@
 	$(hide) $(call assert-max-image-size,$@,$(BOARD_RECOVERYIMAGE_PARTITION_SIZE))
